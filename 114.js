460b1ab9336ff1ae2cc6c75163b2a4031c65afee
andrew-matt <andrew.matt@ya.ru>
2022-05-09 12:26:34 +0300

    Create 114.js

---
+++ 114.js
@@ -0,0 +1,12 @@
+function solution(number){
+  let arr = []
+  if (number < 0) {
+    return 0
+  }
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      arr.push(i)
+    } 
+  }
+  return arr.reduce((acc, cur) => acc + cur, 0)
+}
